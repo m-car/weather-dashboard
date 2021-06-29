36c1bb7e8e782f03509ed08b9a70e472767622bb
Marko Caric <pbmc117@gmail.com>
2021-06-28 19:40:21 -0700

    JS: Initial Weather data from API appends to HTML successfuly.

--- assets/script.js
+++ assets/script.js
@@ -7,12 +7,41 @@ var apiURL = "pro.openweathermap.org/data/2.5/forecast/hourly?";
 var city ="austin"
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+
+var weatherDisplay = document.querySelector(".weather-display");
+
 fetch(queryURL)
 .then(function (response){
+    // console.log(response);
     return response.json();
 })
 .then(function (data){
     console.log(data);
+    //HTML Element Creation
+    var stateName = document.createElement('h2');
+    var stateTemp = document.createElement('p');
+    var stateWind = document.createElement('p');
+    var stateHumidity = document.createElement('p');
+    var stateUVindex = document.createElement('p');
+    
+    //API Data Assignment
+    stateName.textContent = data.name;
+    stateTemp.textContent = "Temp:" + data.main.temp;
+    stateWind.textContent = "Wind:" + data.wind.speed;
+    stateHumidity.textContent = "Humidity:" + data.main.humidity;
+    // stateUVindex.textContent = data.;
+    console.log(stateName.text);
+    
+    //Append to HTML doc
+    weatherDisplay.appendChild(stateName);
+    weatherDisplay.appendChild(stateTemp);
+    weatherDisplay.appendChild(stateWind);
+    weatherDisplay.appendChild(stateHumidity);
+    weatherDisplay.appendChild(stateUVindex);
+
+
+
+
 })
 
 
